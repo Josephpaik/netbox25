3b5071af8196dc15e37713043fddfd0b4575b66e
Joseph paik <josephpaik2008@gmail.com>
2025-11-14 17:01:36 +0900

    Add PostgreSQL user and password to config
    root 계정 + 패스워드 추가함

--- netbox/netbox/configuration_testing.py
+++ netbox/netbox/configuration_testing.py
@@ -11,6 +11,8 @@ DATABASES = {
         'NAME': 'netbox',
         'USER': 'netbox',
         'PASSWORD': 'netbox1234!',
+        'POSTGRES_USER': 'root',
+        'POSTGRES_PASSWORD': 'netbox1234!',
         'HOST': 'localhost',
         'PORT': '',
         'CONN_MAX_AGE': 300,
